d735e37810d1d7812fbb1c8c49c19b52f1ce9795
Ham-J <hamonjuliane@gmail.com>
2025-08-04 09:10:39 +0200

    ajout de la connexion à la base de donnée et server + app

---
+++ back-end/server.js
@@ -0,0 +1,16 @@
+const sequelize = require("./config/db");
+const app = require("./app");    
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+sequelize.authenticate()
+  .then(() => {
+    console.log("MySQL connected");
+    app.listen(PORT, () => {
+      console.log(` Serveur lancé sur http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error("Connexion error :", err));
