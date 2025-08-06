a63cf4e298313f5a2873adfcf76a2cbab83f0227
Ham-J <hamonjuliane@gmail.com>
2025-08-06 21:38:21 +0200

    ajout description

--- front-end/src/pages/NotFound.jsx
+++ front-end/src/pages/NotFound.jsx
@@ -2,9 +2,11 @@ import image404 from "../assets/img/image_404.png";
 
 export default function NotFound() {
   return (
+    <>
     <div className="notfound-container text-center">
       <img src={image404} alt="Page non trouvée" />
       <p>Erreur 404 : page non trouvée</p>
     </div>
+   </> 
   );
 }
